2002306192a31c434eacd7b37f7ab261589fde61
kinsman646 <teesam2004@gmail.com>
2023-10-03 14:52:46 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,48 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Add the .hidden class to the error modal initially
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+
+  // Function to toggle the heart states
+  function toggleHeart(heart) {
+    if (heart.textContent === EMPTY_HEART) {
+      heart.textContent = FULL_HEART;
+      heart.classList.add("activated-heart");
+    } else {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    }
+  }
+
+  // Function to handle like button click
+  function likeButtonClick(event) {
+    const likeButton = event.target;
+    mimicServerCall()
+      .then(() => {
+        toggleHeart(likeButton);
+      })
+      .catch((error) => {
+        const modalMessage = document.getElementById("modal-message");
+        modalMessage.textContent = error;
+        errorModal.classList.remove("hidden");
+
+// Hide the error modal after 3 seconds
+setTimeout(() => {
+  errorModal.classList.add("hidden");
+}, 3000);
+  });
+  }
+
+  // Attach click event listeners to all like buttons
+  const likeButtons = document.querySelectorAll(".like");
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", likeButtonClick);
+  });
+});
+
 
 
 
